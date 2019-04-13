249194d2e5ecb2189d3c791dd27540617036bab8
Gonzalo <gonzalo.sanchez@vilt-group.com>
2019-04-13 20:35:36 +0200

    Todo parece que vuelve a la normalidad

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,24 @@ Route::get('/actualizar', function(){
 
 
 });
+
+Route::get('/borrar', function(){
+
+    //$club=\App\Club::find(5);
+
+    //$club->delete();
+
+    //\App\Club::where('localidad','Gijon')->delete();
+
+});
+
+Route::get('/insertVarios', function(){
+
+    \App\Club::create(["nombre"=>'Club4',"email"=>'club4@gmail.com',"password"=>'club4',"localidad"=>'Tineo',"telefono"=>"999888777",
+        "CIF"=>'ASD123456']);
+
+
+});*/
 /* RELACION 1:N Club->Torneos */
 Route::get('/torneos', function(){
 
@@ -138,21 +156,4 @@ Route::get('/inscripcion/{id}/jugador', function($id){
 
 });
 
-Route::get('/borrar', function(){
-
-    //$club=\App\Club::find(5);
-
-    //$club->delete();
-
-    //\App\Club::where('localidad','Gijon')->delete();
-
-});
-
-Route::get('/insertVarios', function(){
-
-    \App\Club::create(["nombre"=>'Club4',"email"=>'club4@gmail.com',"password"=>'club4',"localidad"=>'Tineo',"telefono"=>"999888777",
-        "CIF"=>'ASD123456']);
-
-
-});
 
